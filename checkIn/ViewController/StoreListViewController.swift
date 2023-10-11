1a517789303c11e573b873a7dc24d47f0cf6bc04
Kobe <dev.skyachieve91@gmail.com>
2023-10-11 13:53:24 +0900

    :recycle: Refactor: view background color 변경.
    systemBackground로 변경하였습니다.

--- checkIn/ViewController/StoreListViewController.swift
+++ checkIn/ViewController/StoreListViewController.swift
@@ -12,7 +12,7 @@ class StoreListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
     }
     
 
